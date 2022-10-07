25dd9d2d0ab8fbb037091984e89a517a64e27bfd
bielzita <35502905+bielzita@users.noreply.github.com>
2022-10-07 23:06:16 +0100

    db modeling
    implemented properties and relationships on db model

--- app.py
+++ app.py
@@ -23,30 +23,45 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# TODO: connect to a local postgresql database : DONE
+# TODO: DONE - connect to a local postgresql database - DONE
 
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
 
+class Show(db.Model):
+  __tablename__ = 'Show'
+
+  id = db.Column(db.Integer, primary_key=True)
+  venue_id = db.Column('venue_id', db.Integer, db.ForeignKey('Venue.id'), primary_key=True)
+  artist_id = db.Column('artist_id', db.Integer, db.ForeignKey('Artist.id'), primary_key=True)
+  startTime = db.Column(db.DateTime, default=datetime.utcnow)
+
+
 class Venue(db.Model):
     __tablename__ = 'Venue'
 
     id = db.Column(db.Integer, primary_key=True)
+    show_id = db.relationship('Show', backref='Venue')
+
     name = db.Column(db.String)
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
+    site = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    seeking_talent = db.Boolean(db.Boolean())
+    seeking_description = db.Column(db.String(120))
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
 
     id = db.Column(db.Integer, primary_key=True)
+    show_id = db.relationship('Show', backref='Artist')
+
     name = db.Column(db.String)
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
@@ -54,10 +69,9 @@ class Artist(db.Model):
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+    seeking_venue = db.Boolean(db.Boolean())
+    seeking_description = db.Column(db.String(120))
+    
 
 #----------------------------------------------------------------------------#
 # Filters.
